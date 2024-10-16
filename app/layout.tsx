fc1c72fff8d5c937850a5e473168193414091d73
Kaustubh <kaustubh8856@gmail.com>
2024-10-16 23:23:20 +0530

    implement Clerk Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Variable } from "lucide-react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +27,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} bg-dark-2 ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <ClerkProvider appearance={{
+        layout:{
+          logoImageUrl:'/icons/yoom-logo.svg',
+          socialButtonsVariant:'iconButton'
+        },
+        variables:{
+          colorText:'#fff',
+          colorPrimary:'#0E78F9',
+          colorBackground:'#1c1f2e',
+          colorInputBackground:'#252a41',
+          colorInputText:'#fff'
+
+        }
+      } }>
+        <body
+          className={`${geistSans.variable} bg-dark-2 ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
