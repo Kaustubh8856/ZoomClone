910db77574729c98423e8d69936d2f08147e8f97
Kaustubh <kaustubh8856@gmail.com>
2024-10-18 10:52:24 +0530

    implement HomePage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import MeetingTypeList from "@/components/MeetingTypeList";
+import React from "react";
 
 const Home = () => {
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
   return (
-    <section className='flex flex-col text-white gap-10 size-full'>
-      <h1 className="text-xl font-bold">Home</h1>
+    <section className="flex flex-col text-white gap-10 size-full">
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 ">
+          <h2 className="glassmorphism max-w-[270px] rounded text-center text-base py-2">
+            Upcoming meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
 
+      <MeetingTypeList/>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
